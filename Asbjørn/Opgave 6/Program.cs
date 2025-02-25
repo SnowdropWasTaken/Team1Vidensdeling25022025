1606684bd732caf4076d05fa557bee848be43405
Asbjørn Brun Hansen <Asbjorn@brun-hansen.eu>
2025-02-25 12:35:36 +0100

    Tilføjelse af opgave 6

---
+++ Asbjørn/Opgave 6/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Opgave_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double i = 1;
+
+            bool isEqual = i == 2;
+            bool isNotEqual = i != 2;
+            bool isGreaterThan = i > 2;
+            bool isLessThan = i < 2;
+            bool isGreaterThanOrEqual = i >= 2;
+            bool isLessThanOrEqual = i <= 1;
+
+            Console.WriteLine("Is equal: " + isEqual + ", " + "Is not equal: " + isNotEqual + ", " + "Is greater: " + isGreaterThan + ", " + "Is less: " + isLessThan + ", " + "Is greater or equal: " + isGreaterThanOrEqual + ", " + "Is less or equal: " + isLessThanOrEqual);
+        }
+    }
+}
