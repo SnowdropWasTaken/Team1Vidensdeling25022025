8358969cc58e41eddc5decd412f862ede5854189
SnowdropWasTaken <156704928+SnowdropWasTaken@users.noreply.github.com>
2025-02-25 12:04:37 +0100

    Add files via upload

---
+++ Asbjørn/Opgave 10/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Opgave_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indtast et tal fra 1 til 7 og få en ugedag :-) : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            switch (n)
+            {
+                case 1:
+                    Console.WriteLine("Mandag");
+                    break;
+                case 2:
+                    Console.WriteLine("Tirsdag");
+                    break;
+                case 3:
+                    Console.WriteLine("Onsdag");
+                    break;
+                case 4:
+                    Console.WriteLine("Torsdag");
+                    break;
+                case 5:
+                    Console.WriteLine("Fredag");
+                    break;
+                case 6:
+                    Console.WriteLine("Lørdag");
+                    break;
+                case 7:
+                    Console.WriteLine("Søndag");
+                    break;
+                default:
+                    Console.WriteLine("Ugyldigt tal");
+                    break;
+            }
+            }
+    }
+}
---
+++ Asbjørn/Opgave 9/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Opgave_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv et årstal for at tjekke om det er et skudår: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0)
+                {
+                    if (year % 400 == 0)
+                    {
+                        Console.WriteLine("Skudår");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ikke skudår");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Skudår");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ikke skudår");
+            }
+        }
+    }
+}
