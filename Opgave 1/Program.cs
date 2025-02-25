313276dd29e8052daf999de413550b157dcc709b
SnowdropWasTaken <156704928+SnowdropWasTaken@users.noreply.github.com>
2025-02-25 11:56:44 +0100

    Add files via upload

---
+++ Opgave 1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Opgave_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 25;
+            double height = 1.75;
+            double weight = 70.5;
+            char initial = 'A';
+            string name = "Alice";
+            bool isStudent = true;
+
+            System.Console.WriteLine("Age: " + age + ", " + "Height: " + height + ", " + "Weight: " + weight + ", " + "Initial: " + initial + ", " + "Name: " + name + ", " + "Is Student: " + isStudent);
+        }
+    }
+}
